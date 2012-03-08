f57c02eea0bbfc0f3f4c006816dc333779ad0083
U-Kramer-Laptop\Kramer <Kramer@Kramer-Laptop.(none)>
2012-03-07 20:33:48 -0800

    updated identifier1.cu to match what we needed and input with memory arrays

--- src/identify1.cu
+++ src/identify1.cu
@@ -5,19 +5,31 @@
 #define BASE_WIDTH = 8
 #define BASE_HEIGHT = 2
 #define THRESHOLD = 0.85 //definitely needs to be changed
+#define SKIP_AMOUNT = 4 //amount to skip in pixels, we can change this to be multiplied by scale if necessary/desirable
 
 __global__ 
-void ID1kernel(int startX, int startY, int scale, float* intImage ) {
-    // take important corners from image
-	int upperLeft 		= intImage[startX][startY];
-	int upperRight 	= intImage[startX+BASE_WIDTH*scale][startY];
-	int midLeft 		= intImage[startX][startY+BASE_HEIGHT*scale];
-	int midRight 		= intImage[startX+BASE_WIDTH*scale][startY+BASE_HEIGHT*scale];
-	int lowerLeft 		= intImage[startX][startY+BASE_HEIGHT*scale<<1];
-	int lowerRight 	= intImage[startX+BASE_WIDTH*scale][startY+BASE_HEIGHT*scale<<1];
-	
-	//calculate fit value based on identifier (hard-coded)
-	int fitValue = midRight<<1-midLeft<<1 + upperLeft - lowerRight - upperRight + lowerLeft;
-	float goodnessValue = fitValue*1.0f/(BASE_WIDTH*scale*BASE_HEIGHT*scale<<1); // goodnessValue = fit/area
+void ID1kernel(int* xVals, int* yVals, int windowSize, int scale, float* intImage, size_t stride, bool* results ) {
+	//Does the intImage need to be float** so we can address into it twice or is this how it works in Cuda
 
+	int threadNum = blockIdx.x * blockDim.x + threadIdx.x;
+	int startX = xVals[threadNum];
+	int startY = yVals[threadNum];
+	for (int i = startX; i < startX+windowSize; i = i+SKIP_AMOUNT){ //use SKIP_AMOUNT * scale for it to scale up as identifier scales
+		for (int j = startY; j < startY + windowSize; j = j+SKIP_AMOUNT){
+			// take important corners from image
+			int upperLeft 		= intImage[i*stride + j];
+			int upperRight 		= intImage[(i+BASE_WIDTH*scale)*stride + j];
+			int midLeft 		= intImage[i*stride + j+BASE_HEIGHT*scale];
+			int midRight 		= intImage[(i+BASE_WIDTH*scale)*stride + j+BASE_HEIGHT*scale];
+			int lowerLeft 		= intImage[i*stride + j+BASE_HEIGHT*scale<<1];
+			int lowerRight 		= intImage[(i+BASE_WIDTH*scale)*stride + j+BASE_HEIGHT*scale<<1];
+			
+			//calculate fit value based on identifier (hard-coded)
+			int fitValue = midRight<<1-midLeft<<1 + upperLeft - lowerRight - upperRight + lowerLeft;
+			float goodnessValue = fitValue*1.0f/(BASE_WIDTH*scale*BASE_HEIGHT*scale<<1); // goodnessValue = fit/area
+			
+			results[i*stride + j] = (goodnessValue>THRESHOLD);
+		}
+	}
+    
 }
